e3c3e95b9d7d667f1ad98a8dd4515834b6c5e09a
jackforest2014 <785748374@qq.com>
2014-05-22 22:12:57 +0800

    first commit of bluetoothtest

---
+++ BluetoothTest2/Assets/Resources/Scripts/BluetoothTest.cs
@@ -0,0 +1,78 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BluetoothTest : MonoBehaviour {
+	AndroidJavaClass oJC;// = new AndroidJavaClass("com.unity3d.player.UnityPlayer");
+	AndroidJavaObject oJO;// = oJC.GetStatic<AndroidJavaObject>("currentActivity");
+
+	string oReturnStr = "@@";
+	string sDebugInfo = "Debug: ";
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.Escape) || Input.GetKeyDown(KeyCode.Home) )
+		{
+			Application.Quit();
+		}
+	}
+
+	void OnGUI(){
+		if(GUILayout.Button("OPEN Activity01",GUILayout.Height(100)))
+		{
+			using (AndroidJavaClass jc = new AndroidJavaClass("com.unity3d.player.UnityPlayer"))
+			{
+				using( AndroidJavaObject jo = jc.GetStatic<AndroidJavaObject>("currentActivity"))
+				{
+					if(jo != null){
+						sDebugInfo += "init success";
+					}
+					//调用Android插件中UnityTestActivity中StartActivity0方法，stringToEdit表示它的参数
+					jo.Call("startDiscovery");
+
+				}
+				
+			}
+			/*
+			AndroidJavaClass jc = new AndroidJavaClass("com.unity3d.player.UnityPlayer");
+			AndroidJavaObject jo = jc.GetStatic<AndroidJavaObject>("currentActivity");
+			jo.Call("StartActivity0","第一个Activity");
+			*/
+		}
+
+		/*
+		if(GUILayout.Button("OPEN Activity02",GUILayout.Height(100)))
+		{
+			//AndroidJavaClass jc = new AndroidJavaClass("com.unity3d.player.UnityPlayer");
+			//AndroidJavaObject jo = jc.GetStatic<AndroidJavaObject>("currentActivity");
+			//jo.Call("StartActivity1","第二个Activit");
+			oJC = new AndroidJavaClass("com.unity3d.player.UnityPlayer");
+			if (oJC == null) {
+				sDebugInfo += "null AndroidJavaClass\n";
+			} else {
+				sDebugInfo += "success new AndroidJavaClass\n";
+			}
+			oJO = oJC.GetStatic<AndroidJavaObject>("currentActivity");
+			if (oJO == null) {
+				sDebugInfo += "Debug: null AndroidJavaObject\n";
+			} else {
+				sDebugInfo += "Debug: sucess AndroidJavaObject\n";
+			}
+			
+			int nRes = oJO.Call<int>("Max", new object[]{10, 20});
+			oReturnStr = nRes.ToString ();
+		}
+
+
+		//if (GUI.Button (new Rect (100, 200, 60, 40), "hehe")) {
+		//	oJO.Call("FindBluetoothDevice");
+		//}
+		GUI.Box (new Rect (100, 100, 200, 60), oReturnStr);*/
+		GUI.Box (new Rect (0, Screen.height - 120, Screen.width, 100), sDebugInfo);
+
+	}
+}
---
+++ BluetoothTest2/Assets/Resources/Scripts/main.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class main : MonoBehaviour {
+	private AndroidJavaObject m_Activity;
+	private int m_nState = -1;
+	private int m_nToyState = 100;
+	private string m_sStr = "gege";
+	private string m_sToyName = "yaya";
+	private bool m_bStartDiscovery = false;
+
+	void Start()
+	{
+		AndroidJavaClass jc = new AndroidJavaClass("com.unity3d.player.UnityPlayer");
+		m_Activity = jc.GetStatic<AndroidJavaObject>("currentActivity");
+	}
+
+	void Update()
+	{
+		if (!m_bStartDiscovery) {
+			m_Activity.Call ("startDiscovery");
+		} else {
+			m_sStr = m_Activity.Call<string>("getStr");
+			m_nState = m_Activity.Call<int>("getBTState");
+			m_nToyState = m_Activity.Call<int>("getToyState");
+			m_sToyName = m_Activity.Call<string>("getToyName");
+		}
+	}
+
+	void OnGUI()
+	{
+		GUI.Label(new Rect(0, 0, 400, 100), "回调函数测试：" + m_sStr);
+		GUI.Label(new Rect(0,110,400,100),"蓝牙状态测试：" + m_nState.ToString());
+		GUI.Label(new Rect(0,220,400,100),"玩具状态:" + m_nToyState);
+		GUI.Label(new Rect(0,330,400,100),"玩具名称:" + m_sToyName);
+	}
+}
