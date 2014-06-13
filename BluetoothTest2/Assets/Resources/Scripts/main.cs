15504b2e952f940011bc95f7412d87980dbcf886
Orange Wong <orange.work@qq.com>
2014-06-13 09:52:04 +0800

    增加了.gitgnore，更换了Android工程文件

--- BluetoothTest2/Assets/Resources/Scripts/main.cs
+++ BluetoothTest2/Assets/Resources/Scripts/main.cs
@@ -2,36 +2,116 @@
 using System.Collections;
 
 public class main : MonoBehaviour {
-	private AndroidJavaObject m_Activity;
-	private int m_nState = -1;
-	private int m_nToyState = 100;
-	private string m_sStr = "gege";
-	private string m_sToyName = "yaya";
-	private bool m_bStartDiscovery = false;
-
+	
+	// Use this for initialization
+	private AndroidJavaObject activity;
+	private int  bstate=-1;
+	
+	private int  toystate = -1;
+	private string str = "gege";
+	private string toyname = "yaya";
+	
+	private bool bStateMinus1, bState0;
+	private bool bToyState0, bToyState3;
+	private int nCount = 0;
+	// private string result="http://blog.csdn.net/dingxiaowei2013";  //result用于接收二维码的返回值
 	void Start()
 	{
 		AndroidJavaClass jc = new AndroidJavaClass("com.unity3d.player.UnityPlayer");
-		m_Activity = jc.GetStatic<AndroidJavaObject>("currentActivity");
+		activity = jc.GetStatic<AndroidJavaObject>("currentActivity");
+		
+		bStateMinus1 = bState0 = false;
+		bToyState0 = bToyState3 = false;
+		str = activity.Call<string>("startDiscovery");
 	}
-
+	
 	void Update()
 	{
-		if (!m_bStartDiscovery) {
-			m_Activity.Call ("startDiscovery");
-		} else {
-			m_sStr = m_Activity.Call<string>("getStr");
-			m_nState = m_Activity.Call<int>("getBTState");
-			m_nToyState = m_Activity.Call<int>("getToyState");
-			m_sToyName = m_Activity.Call<string>("getToyName");
-		}
+		
+		// activity.Call("bondAndConnectDevice");
+		// activity.Call("startDiscovery");
+		//state =  activity.Call<int>("getBTState");
+		/***str = activity.Call<string>("getStr");
+	   bstate = activity.Call<int>("getBTState");
+	    toystate = activity.Call<int>("getToyState");
+       toyname =  activity.Call<string>("getToyName");
+       ***/
+		//activity.Call("startDiscovery");
+		bstate = activity.Call<int>("getBTState");	   
+		toystate = activity.Call<int>("getToyState");
+		//toyname =  activity.Call<string>("getToyName");
+		//str = nCount.ToString();
+		++nCount;
+		if(nCount > 0x7FFFFFFE)
+			nCount = 0;
+		/*if(!bState0)
+		{
+			activity.Call("bond");
+			bState0 = true;
+		}*/
+		/*switch(bstate)
+	   {
+		case -1:
+			if(!bStateMinus1)
+			{
+				activity.Call("setBluetoothEnabled", true);
+				bStateMinus1 = true;
+			}
+			break;
+		case 0: //蓝牙已打开
+			if(!bState0)
+			{
+				activity.Call("bond");
+				bState0 = true;
+			}
+			break;
+		default:
+			if(!bState0)
+			{
+				activity.Call("bond");
+				bState0 = true;
+			}
+			break;
+	   };*/
+		
+		/*if(toystate != 0)
+	   {
+			if(!bToyState0)
+			{
+				activity.Call("connect");
+				bToyState0 = true;
+			}
+	   }*/
+		/*switch(toystate)
+	   {
+		case 0: //已配对
+			if(!bToyState0)
+			{
+				activity.Call("connect");
+				bToyState0 = true;
+			}
+			break;
+		case 3: //已连接
+			if(!bToyState3)
+			{
+				bToyState3 = true;
+				toyname =  activity.Call<string>("getToyName");
+			}
+			break;
+	   }*/
+		
 	}
-
 	void OnGUI()
 	{
-		GUI.Label(new Rect(0, 0, 400, 100), "回调函数测试：" + m_sStr);
-		GUI.Label(new Rect(0,110,400,100),"蓝牙状态测试：" + m_nState.ToString());
-		GUI.Label(new Rect(0,220,400,100),"玩具状态:" + m_nToyState);
-		GUI.Label(new Rect(0,330,400,100),"玩具名称:" + m_sToyName);
+		
+		GUI.Button(new Rect(0, 0, 400, 100), "回调函数测试："+str);
+		GUI.Button(new Rect(100,100,400,100),"蓝牙状态测试："+bstate.ToString());
+		GUI.Button(new Rect(200,200,400,100),"玩具状态:"+toystate.ToString());
+		//GUI.Button(new Rect(300,300,400,100),"玩具名称:"+toyname);
+		if(bState0)
+			GUI.Button(new Rect(0, 420, 400, 100), "Start bond: true");
+		else
+			GUI.Button(new Rect(0, 420, 400, 100), "Start bond: false");
 	}
+	
 }
