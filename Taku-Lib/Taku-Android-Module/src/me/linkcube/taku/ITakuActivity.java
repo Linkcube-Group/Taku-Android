6bad79603131633f85b1fd85b7d80eef70f7c48a
Ervin Wang <orange.work@qq.com>
2014-08-06 11:07:04 +0800

    增加响应Unity3D调用的接口

--- Taku-Lib/Taku-Android-Module/src/me/linkcube/taku/ITakuActivity.java
+++ Taku-Lib/Taku-Android-Module/src/me/linkcube/taku/ITakuActivity.java
@@ -2,4 +2,10 @@ package me.linkcube.taku;
 
 public interface ITakuActivity {
 
+	public String getData();
+
+	public String getDeviceNames();
+
+	public String getHexData();
+
 }
--- Taku-Lib/Taku-Android-Module/src/me/linkcube/taku/TakuActivity.java
+++ Taku-Lib/Taku-Android-Module/src/me/linkcube/taku/TakuActivity.java
@@ -8,7 +8,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 
-public class TakuActivity extends UnityPlayerActivity {
+public class TakuActivity extends UnityPlayerActivity implements ITakuActivity {
 
 	Context mContext = null;
 
@@ -21,6 +21,24 @@ public class TakuActivity extends UnityPlayerActivity {
 	public void startBTSettingActivity(String name) {
 		Intent intent = new Intent(mContext, BTSettingActivity.class);
 		this.startActivity(intent);
+	}
+
+	@Override
+	public String getData() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getDeviceNames() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getHexData() {
+		// TODO Auto-generated method stub
+		return null;
 	}
 
 }
