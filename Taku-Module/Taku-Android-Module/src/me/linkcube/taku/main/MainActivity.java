ddc5834d73ac8de2784ca5d9f2f3e31b6e9e9ced
Ervin Wang <orange.work@qq.com>
2014-08-23 14:10:18 +0800

    增加程序新入口

---
+++ Taku-Module/Taku-Android-Module/src/me/linkcube/taku/main/MainActivity.java
@@ -0,0 +1,117 @@
+package me.linkcube.taku.main;
+
+import me.linkcube.taku.R;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.ActionBar;
+import android.support.v4.app.FragmentManager;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.support.v4.widget.DrawerLayout;
+
+public class MainActivity extends ActionBarActivity implements
+		NavigationDrawerFragment.NavigationDrawerCallbacks {
+
+	private NavigationDrawerFragment mNavigationDrawerFragment;
+
+	private CharSequence mTitle;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main_activity);
+
+		mNavigationDrawerFragment = (NavigationDrawerFragment) getSupportFragmentManager()
+				.findFragmentById(R.id.navigation_drawer);
+
+		mNavigationDrawerFragment.setUp(R.id.navigation_drawer,
+				(DrawerLayout) findViewById(R.id.drawer_layout));
+	}
+
+	@Override
+	public void onNavigationDrawerItemSelected(int position) {
+		FragmentManager fragmentManager = getSupportFragmentManager();
+		switch (position) {
+		case 0:
+
+			fragmentManager
+					.beginTransaction()
+					.replace(R.id.container, SportsGamesFragment.newInstance(position))
+					.commit();
+			break;
+		case 1:
+			fragmentManager = getSupportFragmentManager();
+			fragmentManager
+					.beginTransaction()
+					.replace(R.id.container, SportsGamesFragment.newInstance(position))
+					.commit();
+			break;
+		case 2:
+			fragmentManager = getSupportFragmentManager();
+			fragmentManager
+					.beginTransaction()
+					.replace(R.id.container, SportsGamesFragment.newInstance(position))
+					.commit();
+			break;
+
+		case 3:
+			fragmentManager = getSupportFragmentManager();
+			fragmentManager
+					.beginTransaction()
+					.replace(R.id.container, SportsGamesFragment.newInstance(position))
+					.commit();
+			break;
+		default:
+			break;
+		}
+
+	}
+
+	public void onSectionAttached(int number) {
+		switch (number) {
+		case 0:
+			mTitle = getString(R.string.sports_games);
+			break;
+		case 1:
+			mTitle = getString(R.string.history);
+			break;
+		case 2:
+			mTitle = getString(R.string.network_games);
+			break;
+		case 3:
+			mTitle = getString(R.string.settings);
+			break;
+		default:
+			break;
+		}
+		restoreActionBar();
+	}
+
+	public void restoreActionBar() {
+		ActionBar actionBar = getSupportActionBar();
+		actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_STANDARD);
+		actionBar.setDisplayShowTitleEnabled(true);
+		actionBar.setTitle(mTitle);
+	}
+
+	// TODO ���Menu��Ȼ�󵯳�����Drawer
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		if (!mNavigationDrawerFragment.isDrawerOpen()) {
+			getMenuInflater().inflate(R.menu.main, menu);
+			restoreActionBar();
+			return true;
+		}
+		return super.onCreateOptionsMenu(menu);
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+
+}
---
+++ Taku-Module/Taku-Android-Module/src/me/linkcube/taku/main/SportsGamesFragment.java
@@ -0,0 +1,72 @@
+package me.linkcube.taku.main;
+
+import me.linkcube.taku.R;
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+public class SportsGamesFragment extends Fragment implements OnClickListener {
+
+	private static final String ARG_SECTION_NUMBER = "section_number";
+
+	private Button dashboardBtn;
+
+	private Button takuBtn;
+
+	public static SportsGamesFragment newInstance(int sectionNumber) {
+		SportsGamesFragment fragment = new SportsGamesFragment();
+		Bundle args = new Bundle();
+		args.putInt(ARG_SECTION_NUMBER, sectionNumber);
+		fragment.setArguments(args);
+		return fragment;
+	}
+
+	public SportsGamesFragment() {
+	}
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		View rootView = inflater.inflate(R.layout.main_fragment, container,
+				false);
+		return rootView;
+	}
+
+	@Override
+	public void onViewCreated(View view, Bundle savedInstanceState) {
+		super.onViewCreated(view, savedInstanceState);
+		dashboardBtn = (Button) view.findViewById(R.id.dashboardBtn);
+		takuBtn = (Button) view.findViewById(R.id.takuBtn);
+		dashboardBtn.setOnClickListener(this);
+		takuBtn.setOnClickListener(this);
+	}
+
+	@Override
+	public void onAttach(Activity activity) {
+		super.onAttach(activity);
+		((MainActivity) activity).onSectionAttached(getArguments().getInt(
+				ARG_SECTION_NUMBER));
+
+	}
+
+	@Override
+	public void onClick(View v) {
+
+		switch (v.getId()) {
+		case R.id.dashboardBtn:
+			// TODO
+			break;
+		case R.id.takuBtn:
+			// TODO
+			break;
+		default:
+			break;
+		}
+
+	}
+}
