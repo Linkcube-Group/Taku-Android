3a94f656d3410410a54645b9621505839f6a25bf
Ervin <orange.work@qq.com>
2014-09-07 23:08:30 +0800

    增加工具包

--- Taku-Module/Taku-Android-Module/src/me/linkcube/taku/ui/setting/SettingFragment.java
+++ Taku-Module/Taku-Android-Module/src/me/linkcube/taku/ui/setting/SettingFragment.java
@@ -2,6 +2,6 @@ package me.linkcube.taku.ui.setting;
 
 import me.linkcube.taku.common.ui.DialogActivity;
 
-public class SettingActivity extends DialogActivity {
+public class SettingFragment extends DialogActivity {
 
 }
