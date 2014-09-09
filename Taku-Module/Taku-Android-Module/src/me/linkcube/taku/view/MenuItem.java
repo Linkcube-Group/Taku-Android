cbeb9f1e687b64015060de9623e50898c7ea097a
Ervin <orange.work@qq.com>
2014-09-10 07:04:54 +0800

    增加了MenuItem的TipView，用于设置等页面

--- Taku-Module/Taku-Android-Module/src/me/linkcube/taku/view/MenuItem.java
+++ Taku-Module/Taku-Android-Module/src/me/linkcube/taku/view/MenuItem.java
@@ -18,7 +18,9 @@ public class MenuItem extends LinearLayout {
 	/** menu item icon */
 	private ImageView imageView;
 	/** menu item title */
-	private TextView textView;
+	private TextView titleView;
+	/** menu item tip */
+	private TextView tipView;
 
 	public MenuItem(Context context) {
 		this(context, null);
@@ -29,21 +31,31 @@ public class MenuItem extends LinearLayout {
 		super(context, attrs);
 		initViews(context);
 		final Resources res = getResources();
-		final int defaultTextColor = res
-				.getColor(R.color.default_menu_item_textColor);
-		final float defaultTextSize = res
-				.getDimension(R.dimen.default_menu_item_textSize);
-		final int defaultBackground = res
+		final int defaultTitleTextColor = res
+				.getColor(R.color.default_menu_item_titleTextColor);
+		final float defaultTitleTextSize = res
+				.getDimension(R.dimen.default_menu_item_titleTextSize);
+		final int defaultTipTextColor = res
+				.getColor(R.color.default_menu_item_tipTextColor);
+		final float defaultTipTextSize = res
+				.getDimension(R.dimen.default_menu_item_tipTextSize);
+
+		final int defaultTitleBackground = res
 				.getColor(R.color.default_menu_item_background);
 
 		TypedArray a = context.obtainStyledAttributes(attrs,
 				R.styleable.MenuItem);
 
-		textView.setText(a.getString(R.styleable.MenuItem_Text));
-		textView.setTextColor(a.getColor(R.styleable.MenuItem_TextColor,
-				defaultTextColor));
-		textView.setTextSize(a.getDimension(R.styleable.MenuItem_TextSize,
-				defaultTextSize));
+		titleView.setText(a.getString(R.styleable.MenuItem_TitleText));
+		titleView.setTextColor(a.getColor(R.styleable.MenuItem_TitleTextColor,
+				defaultTitleTextColor));
+		titleView.setTextSize(a.getDimension(
+				R.styleable.MenuItem_TitleTextSize, defaultTitleTextSize));
+		tipView.setText(a.getString(R.styleable.MenuItem_TipText));
+		tipView.setTextColor(a.getColor(R.styleable.MenuItem_TipTextColor,
+				defaultTipTextColor));
+		tipView.setTextSize(a.getDimension(R.styleable.MenuItem_TipTextSize,
+				defaultTipTextSize));
 
 		Drawable background = a.getDrawable(R.styleable.MenuItem_Src);
 		if (background != null) {
@@ -54,7 +66,7 @@ public class MenuItem extends LinearLayout {
 			}
 		} else {
 			int bgColor = a.getColor(R.styleable.MenuItem_Background,
-					defaultBackground);
+					defaultTitleBackground);
 			setBackgroundColor(bgColor);
 		}
 
@@ -74,35 +86,29 @@ public class MenuItem extends LinearLayout {
 		LayoutInflater inflater = (LayoutInflater) context
 				.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
 		inflater.inflate(R.layout.abc_meun_item, this);
-		textView = (TextView) findViewById(R.id.textView);
+		titleView = (TextView) findViewById(R.id.textView);
 		imageView = (ImageView) findViewById(R.id.imageView);
+		tipView = (TextView) findViewById(R.id.tipTv);
 	}
 
-	/**
-	 * set the icon color;
-	 * 
-	 * @param icon
-	 */
 	public void setIcon(int icon) {
 		imageView.setImageResource(icon);
 	}
 
-	/**
-	 * set the title with resource ;
-	 * 
-	 * @param title
-	 */
 	public void setTitle(int title) {
-		textView.setText(title);
+		titleView.setText(title);
 	}
 
-	/**
-	 * set the title with string;
-	 * 
-	 * @param title
-	 */
 	public void setTitle(String title) {
-		textView.setText(title);
+		titleView.setText(title);
+	}
+
+	public void setTip(int tip) {
+		tipView.setText(tip);
+	}
+
+	public void setTip(String tip) {
+		tipView.setText(tip);
 	}
 
 }
