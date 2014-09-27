6ee80dc057ded625d466d8646dc4f0b269753c02
Ervin Wang <orange.work@qq.com>
2014-09-27 15:21:20 +0800

    增加ActivityUtils和Toaster

--- Taku-Android/Taku/src/me/linkcube/taku/ui/setting/SettingFragment.java
+++ Taku-Android/Taku/src/me/linkcube/taku/ui/setting/SettingFragment.java
@@ -10,6 +10,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.Toast;
 import me.linkcube.taku.R;
 import me.linkcube.taku.AppConst.ErrorFlag;
 import me.linkcube.taku.ui.user.HttpResponseListener;
